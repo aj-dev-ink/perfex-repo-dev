54567dbbfe64a9443ea97e8b462d9ffa98fc5617
glskunal <kunal@khandarsoftech.com>
2024-10-03 21:47:12 +0530

    committed latest send mail form changes

--- application/views/admin/workflow/actions/sendemail.php
+++ application/views/admin/workflow/actions/sendemail.php
@@ -10,44 +10,8 @@
                 <div id="myDivContent">
                     <div class="row">
                         <div class="col-sm-12">
-                            <div class="form-group" app-field-wrapper="sendEmailFrom">
-                                <label for="sendFrom_multiselect"><?php echo 'From'; ?></label>
-                                <select id="sendEmailFrom" name="sendEmailFrom[]" class="form-control selectpicker" multiple="multiple" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
-                                    <option value="option1"><?php echo _l('option_1'); ?></option>
-                                    <option value="option2"><?php echo _l('option_2'); ?></option>
-                                    <option value="option3"><?php echo _l('option_3'); ?></option>
-                                </select>
-                            </div>
-                            <div class="form-group sendEMailTo" app-field-wrapper="sendEmailTo">
-                                <?php //$value = (isset($workflow) ? $workflow->description : ''); ?>
-                                <?php echo render_input('sendTo', 'To', ''); ?>
-                                <div class="input-group-append addCCBCC">
-                                    <button id="addCc" class="addCc-btn" type="button">+CC</button>
-                                    <button id="addBcc" class="addBcc-btn" type="button">+BCC</button>
-                                </div>
-                            </div>
-
-                            <!-- Add CC Field -->
-                            <div id="ccField" style="display:none;">
-                                <?php echo render_input('cc', 'CC', ''); ?>
-                            </div>
-                            <!-- Add BCC Field -->
-                            <div id="bccField" style="display:none;">
-                                <?php echo render_input('bcc', 'BCC', ''); ?>
-                            </div>
-
-                            <div class="form-group" app-field-wrapper="sendEmailChooseTemplate">
+                        <div class="form-group" app-field-wrapper="sendEmailChooseTemplate">
                                 <label for="sendFrom_multiselect"><?php echo 'Choose Template'; ?></label>
-                                <!-- <select class="form-control" id="sendEmailTemplate" name="sendEmailTemplate">
-                                    <option value="-">Select</option>
-                                        <?php /*foreach( $emailTemplate as $label=>$value ) { ?>
-                                            <option value="<?php echo $value;?>"
-                                                    <?php if( isset( $workflowSendEmail ) && $value == $workflowSendEmail->emailTemplate ) echo 'selected';?>
-                                                >
-                                                <?php echo $label; ?>
-                                            </option>
-                                        <?php }*/ ?>
-                                </select> -->
                                 <select class="form-control" id="sendEmailAction" name="sendEmail[template_id]">
                                     <option value="-">Select Email Template</option>
                                     <?php foreach( $templates as $objTemplate ) { ?>
@@ -59,14 +23,53 @@
                                     <?php } ?>
                                 </select> 
                             </div>
-                            <div class="form-group" app-field-wrapper="sendEmailSubject">
+
+                            <div class="form-group" app-field-wrapper="sendEmailTo">
+                                <label for="sendFrom_multiselect"><?php echo 'To Email'; ?></label>
+                                <select id="sendEmailTo" name="sendEmail[email_to_fields]" class="form-control selectpicker" multiple="multiple" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                                    <option value="option1"><?php echo _l('option_1'); ?></option>
+                                    <option value="option2"><?php echo _l('option_2'); ?></option>
+                                    <option value="option3"><?php echo _l('option_3'); ?></option>
+                                </select>
+                                
+                            </div>
+                            <!-- <div class="form-group sendEMailTo" app-field-wrapper="sendEmailTo">
                                 <?php //$value = (isset($workflow) ? $workflow->description : ''); ?>
-                                <?php echo render_input('sendEmailSubject', 'Subject', ''); ?>
+                                <?php //echo render_input('sendTo', 'To', ''); ?>
+                                <div class="input-group-append addCCBCC">
+                                    <button id="addCc" class="addCc-btn" type="button">+CC</button>
+                                    <button id="addBcc" class="addBcc-btn" type="button">+BCC</button>
+                                </div>
+                            </div> -->
+                            
+                            <div class="addCCBCC">
+                                <button id="addCc" class="addCc-btn" type="button">+CC</button>
+                            </div>
+                            <!-- Add CC Field -->
+                            <div id="ccField" style="display:none;">
+                                <label for="sendFrom_multiselect"><?php echo 'CC'; ?></label>
+                                <select id="sendEmailTo" name="sendEmail[email_cc_fields]" class="form-control selectpicker" multiple="multiple" data-none-selected-text="<?php echo _l('dropdown_non_selected_tex'); ?>">
+                                    <option value="option1"><?php echo _l('option_1'); ?></option>
+                                    <option value="option2"><?php echo _l('option_2'); ?></option>
+                                    <option value="option3"><?php echo _l('option_3'); ?></option>
+                                </select>
+                                <?php //echo render_input('cc', 'CC', ''); ?>
+                            </div>
+                            
+                            <!-- Add BCC Field 
+                            <div id="bccField" style="display:none;">
+                                <?php //echo render_input('bcc', 'BCC', ''); ?>
+                            </div>-->
+
+                            <!-- <div class="form-group" app-field-wrapper="sendEmailSubject">
+                                <?php //$value = (isset($workflow) ? $workflow->description : ''); ?>
+                                <?php //echo render_input('sendEmailSubject', 'Subject', ''); ?>
                             </div>
                             <div class="form-group" app-field-wrapper="sendEmailBody">
                                 <?php //$value = (isset($workflow) ? $workflow->description : ''); ?>
-                                <?php echo render_textarea('sendEmailBody', 'Body', '', array('rows' => 15)); ?>
-                            </div>
+                                <?php //echo render_textarea('sendEmailBody', 'Body', '', array('rows' => 15)); ?>
+                            </div> -->
+
                         </div>
                     </div>
                 </div>
